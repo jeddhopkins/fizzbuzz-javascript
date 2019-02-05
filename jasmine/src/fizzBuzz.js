1f0cabf7a5dc9921d0576034e11a9e6c9730e0bc
Jedd Hopkins <jedd_hoppo@hotmail.com>
2019-02-05 13:56:26 +0000

    code written, all tests passing

--- jasmine/src/fizzBuzz.js
+++ jasmine/src/fizzBuzz.js
@@ -1,7 +1,15 @@
 function FizzBuzz() {
+}
 
-};
+FizzBuzz.prototype.numbers = function(number) {
+  if (number % 15 === 0) {return 'FizzBuzz';}
+  if (number % 5 === 0) {return 'Buzz';}
+  if (number % 3 === 0) {return 'Fizz';}
+  return number;
+}
 
-FizzBuzz.prototype.numbers = function () {
-  if (fizzBuzz.number)
-};
+var fizzBuzz = new FizzBuzz();
+
+for (var i = 1; i <= 100; i++) {
+  console.log(fizzBuzz.numbers(i));
+}
